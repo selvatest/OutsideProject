96287f66f99e2c09463b5b357ba471e7fe69466b
PRAKASH <PRAKASH@PRAKASH-PC>
2018-01-04 19:47:20 +0530

    changes latest

--- src/main/java/Google/Auzmor/App.java
+++ src/main/java/Google/Auzmor/App.java
@@ -1,23 +1,38 @@
 package Google.Auzmor;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class App 
 {
-    public static void main( String[] args )
+	
+	@Test(dataProvider="datasheet")
+    public void main(String data)
     {
        // System.out.println( "Hello World!" );
-    	System.getProperty("webdriver.chrome.driver","E:\\Room mates work\\M.Selva1\\Softwares\\chromedriver.exe");
+    	System.setProperty("webdriver.chrome.driver","E:\\Room mates work\\M.Selva1\\Softwares\\chromedriver.exe");
     	WebDriver wd= new ChromeDriver();
     	wd.get("http://www.google.com/");
     	WebElement searchbox= wd.findElement(By.xpath("//input[@type=\"text\"]"));
-    	searchbox.sendKeys("Automation testing");
-    	WebElement  button =wd.findElement(By.xpath("//input[@value=\"Google Search\"]"));
+    	searchbox.sendKeys(data);
+    	searchbox.submit();
     	
-    	
-    
-    }
+     }
+	
+
+	@DataProvider(name="datasheet")
+	
+	public  static Object[][] data() throws IOException {
+		
+		//ExcelReader a= new ExcelReader();
+		Object[][]data= ExcelReader.singledata();
+		return data;
+	}
+	
 }
--- src/main/java/Google/Auzmor/ExcelReader.java
+++ src/main/java/Google/Auzmor/ExcelReader.java
@@ -9,33 +9,35 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ExcelReader {
 
 		
-			public static void main(String[] args) throws IOException {
+			/*public static void main(String[] args) throws IOException {
 				
 				ExcelReader a= new ExcelReader();
 				
 				a.singledata();
-			}
+			}*/
 			
 public static  Object[][] singledata() throws IOException
-			//public static void main(String[] args) throws IOException 
+		//	public static void main(String[] args) throws IOException 
 			{
 			String path=System.getProperty("user.dir");
-			FileInputStream fis = new FileInputStream(path+"\\Testdata.xlsx");
+			System.out.println(path);
+			FileInputStream fis = new FileInputStream(path+"\\InputData.xlsx");
 			XSSFWorkbook wb =new XSSFWorkbook(fis);
 			XSSFSheet sh=wb.getSheet("data");
 			int rowcount=sh.getLastRowNum();
 			int colcount=sh.getRow(0).getLastCellNum();
 			Object[][] data= new Object[colcount][rowcount+1];
-			//System.out.println(rowcount);
+			
+			System.out.println(rowcount);
 			for(int i=0;i<=rowcount;i++)
 			{
 				for(int j=0;j<colcount;j++)
 				{
 		        
 				data[j][i]=sh.getRow(i).getCell(j).getStringCellValue();
-				//String data=sh.getRow(i).getCell(j).getStringCellValue();
-		/*   String data=sh.getRow(i).getCell(0).getStringCellValue();*/
-		   System.out.println(data);
+				//String data1=sh.getRow(i).getCell(j).getStringCellValue();
+		//   String data=sh.getRow(i).getCell(0).getStringCellValue();*/
+		   //System.out.println(data1);
 			}
 			}
 //			return null;
@@ -45,28 +47,6 @@ public static  Object[][] singledata() throws IOException
 			
 
 			}	
-			/*public static  Object[][] runtype() throws IOException
 			
-			{
-			FileInputStream fis = new FileInputStream("C:\\Users\\PRAKASH\\workspace\\China\\Testdata.xlsx");
-			XSSFWorkbook wb =new XSSFWorkbook(fis);
-			XSSFSheet sh=wb.getSheet("data");
-			int rowcount=sh.getLastRowNum();
-			int colcount=sh.getRow(0).getLastCellNum();
-		    Object[][] data= new Object[colcount][rowcount+1];
-			//System.out.println(rowcount);
-			for(int i=0;i<=rowcount;i++)
-			{
-		        
-				data[1][i]=sh.getRow(i).getCell(1).getStringCellValue();
-		  // String data=sh.getRow(i).getCell(0).getStringCellValue();
-		   //System.out.println(data);
-			}/
-//			return null;
-			return data;
-			
-			
-			}	*/
-
 			}
 	
